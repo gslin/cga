d610ea4835b0a7aadf8b64b9f68c29c8ec98a420
Gea-Suan Lin <darkkiller@gmail.com>
2010-12-06 04:05:06 +0800

    - 增加關鍵字。

--- CGA.pm
+++ CGA.pm
@@ -6,6 +6,8 @@ use warnings;
 sub parseKeyword {
     my $str = shift;
 
+    return 1 if $str =~ /(?:署|局).*部/;
+    return 1 if $str =~ /(?:通|警).*大/;
     return 1 if $str =~ /(?:海|洋|岸|北|中|南|東).*巡/;
     return 1 if $str =~ /安.*檢/;
     return 1 if $str =~ /(?:一|二|三|四|五|六|七|八|九).*(?:大|總).*隊/;
